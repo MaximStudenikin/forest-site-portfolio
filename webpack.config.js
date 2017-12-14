e301cb5527876423cee1ac1a7425078cf8930821
Maxim Studenikin <operatoribm@gmail.com>
2017-12-14 13:59:36 +0300

    первые потуги:)

---
+++ webpack.config.js
@@ -0,0 +1,92 @@
+const path = require('path');
+const webpack = require('webpack');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+
+
+//пути
+const PATHS = {
+    dev: path.join(__dirname, 'dev'),
+    build: path.join(__dirname, 'build')
+};
+
+module.exports = {
+    entry: {
+        'index' : PATHS.dev + '/pages/index/index.js',
+        'blog'  : PATHS.dev + '/pages/blog/blog.js',
+        'about' : PATHS.dev + '/pages/about/about.js',
+        'works' : PATHS.dev + '/pages/works/works.js'
+    },
+    output: {
+        path: PATHS.build,
+        filename: './js/[name].js'
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            chunks  : ['index', 'common'],
+            template: PATHS.dev + '/pages/index/index.pug'
+        }),
+        new UglifyJSPlugin({
+            sourceMap: true
+        }),
+        new HtmlWebpackPlugin({
+            filename: 'blog.html',
+            chunks  : ['blog', 'common'],
+            template: PATHS.dev + '/pages/blog/blog.pug'
+        }),
+        new HtmlWebpackPlugin({
+            filename: 'about.html',
+            chunks  : ['about', 'common'],
+            template: PATHS.dev + '/pages/about/about.pug'
+        }),
+        new HtmlWebpackPlugin({
+            filename: 'works.html',
+            chunks  : ['works', 'common'],
+            template: PATHS.dev + '/pages/works/works.pug'
+        }),
+        new CleanWebpackPlugin('build'),
+        new ExtractTextPlugin('./css/[name].css'),
+        new webpack.optimize.CommonsChunkPlugin({
+            name: 'common'
+        }),
+        new OptimizeCssAssetsPlugin({
+            cssProcessorOptions: { discardComments: {removeAll: true } }
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.pug$/,
+                loader: 'pug-loader',
+                options: {
+                    pretty: true
+                }
+            },
+            {
+                test: /\.scss$/,
+                use: ExtractTextPlugin.extract({
+                    publicPath: '../',
+                    use: ['css-loader','sass-loader'],
+                })
+            },
+            {
+                test: /\.css$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: 'css-loader'
+                })
+            },
+            {
+                test: /\.(jpg|png|svg)$/,
+                loader: 'file-loader',
+                options: {
+                    name: 'images/[name].[ext]'
+                }
+            }
+        ]
+    }
+};
