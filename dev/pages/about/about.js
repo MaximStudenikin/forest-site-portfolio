618a1ad2f5e131011bad91a4d097a56e34cf2f10
Maxim Studenikin <operatoribm@gmail.com>
2017-12-16 23:14:47 +0300

    Новые проблемы

--- dev/pages/about/about.js
+++ dev/pages/about/about.js
@@ -1,2 +1,10 @@
 import 'normalize.css';
 import '../../common/main.scss'
+
+let thisMap = function() {
+    let uluru = {lat: -25.363, lng: 131.044};
+    let map = new google.maps.Map(document.getElementById('map'), {zoom: 4, center: uluru});
+    let marker = new google.maps.Marker({position: uluru, map: map});
+};
+
+window.thisMap = thisMap;
