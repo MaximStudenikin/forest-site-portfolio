db4ebe063b2f9fcc7c61aa51248d8474e063901b
Maxim Studenikin <operatoribm@gmail.com>
2017-12-17 02:26:26 +0300

    Карта работает

--- dev/pages/about/about.js
+++ dev/pages/about/about.js
@@ -1,10 +1,11 @@
 import 'normalize.css';
 import '../../common/main.scss'
 
-let thisMap = function() {
-    let uluru = {lat: -25.363, lng: 131.044};
-    let map = new google.maps.Map(document.getElementById('map'), {zoom: 4, center: uluru});
-    let marker = new google.maps.Marker({position: uluru, map: map});
+var initMap = () => {
+    var uluru = {lat: -25.363, lng: 131.044};
+    var map = new google.maps.Map(document.getElementById('map'), {zoom: 4, center: uluru});
+    var marker = new google.maps.Marker({position: uluru, map: map});
 };
 
-window.thisMap = thisMap;
+global.initMap = initMap;
+window.initMap = initMap;
