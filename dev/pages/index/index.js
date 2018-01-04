89ad849041a8baa456b7168f91accee6e90f01d4
Maxim Studenikin <operatoribm@gmail.com>
2018-01-04 17:20:26 +0300

    flip effect

--- dev/pages/index/index.js
+++ dev/pages/index/index.js
@@ -2,3 +2,13 @@ import 'normalize.css';
 import '../../common/main.scss';
 // import './woter__webGL/haze';
 // import './woter__webGL/index-water';
+
+let $ = require('jquery');
+
+//flip__effect
+$('.flip__effect').on('click', event =>{
+    event.preventDefault();
+
+    $('.flip__card').toggleClass('flipped__card');
+
+});
