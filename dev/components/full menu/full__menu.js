1f3248d9f4e5ea5ef49a745730299c9cc9cdaedc
Maxim Studenikin <operatoribm@gmail.com>
2018-01-06 23:25:24 +0300

    swipe and more

--- dev/components/full menu/full__menu.js
+++ dev/components/full menu/full__menu.js
@@ -1,26 +1,6 @@
 let $ = require('jquery');
 
-$('.hamburger__menu').on('click', event => {
+$('.hamburger__menu, .full__menu-close').on('click', event => {
     event.preventDefault();
-
     $('.full__menu').toggleClass('visuallyhidden');
 });
-
-$('.full__menu-close').on('click', event => {
-    event.preventDefault();
-
-    $('.full__menu').toggleClass('visuallyhidden');
-});
-
-// hamburgerMenu.addEventListener('click', () => {
-//     fullMenu.classList.toggle('visuallyhidden');
-//
-//     addOnWheel(fullMenu, event => {
-//         let delta = event.deltaY || event.detail || event.wheelDelta;
-//
-//         event.preventDefault();
-//     });
-// });
-// closeFullMenu.addEventListener('click', () => {
-//     fullMenu.classList.toggle('visuallyhidden');
-// });
--- dev/pages/about/about.js
+++ dev/pages/about/about.js
@@ -3,3 +3,4 @@ import 'normalize.css';
 import '../../common/main.scss'
 import '../../components/map/map';
 import '../../components/effects/paralax/paralax';
+import '../../components/full menu/full__menu'
