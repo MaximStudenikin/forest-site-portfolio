81b62920c0273164b5ae08464b975d0445b6e6c0
Maxim Studenikin <operatoribm@gmail.com>
2018-07-26 12:24:03 +0300

    последние изменения

--- dev/pages/blog/blog.js
+++ dev/pages/blog/blog.js
@@ -2,3 +2,22 @@ import 'normalize.css';
 import '../../common/main.scss'
 import '../../components/effects/paralax/paralax';
 import '../../components/full menu/full__menu'
+
+let $ = require('jquery');
+
+
+$('.swipe__sidebar__btn').on('click', event => {
+    event.preventDefault();
+
+    const $this = $(event.target),
+          container = $this.closest('.swipe__sidebar');
+
+    if (!container.hasClass('swipe__sidebar--open')){
+        $('.swipe__sidebar').toggleClass('swipe__sidebar--open');
+        $('.wrap').css('marginLeft' , '290px');
+    } else {
+        $('.swipe__sidebar').toggleClass('swipe__sidebar--open');
+        $('.wrap').css('marginLeft' , '0');
+    }
+
+});
